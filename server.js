e6177547bc74bd58d1620c15c35fb33c702db48b
Mayank Gangrade <gangrademayank91@gmail.com>
2018-11-11 01:33:53 -0500

    Connected server to html file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello world")
-});
+// app.get('/', function(req, res){
+//     res.send("Hello world")
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+
 
 app.listen(3000);
 console.log("server running on port 3000");
