332aa095dc334ae2ab94b46656bd8b49144f79fa
Mayank Gangrade <gangrademayank91@gmail.com>
2018-11-11 03:18:56 -0500

    changes

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,8 +1,13 @@
-var myApp = angular.module('myApp',[]);
-myApp.controller('AppCtrl',['$scope', '$http',
-function($scope, $http){
-    console.log("hello world from controller");
-}]);
-// function AppCtrl(){
-//     console.log("hello from controller")
-// }
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        console.log("hello world from controller");
+
+        $http.get('/contactlist').
+            then(function (response) {
+                //success code
+                console.log("Requested Data Received");
+                console.log(response);
+                $scope.contactlist = response;
+            });
+    }]);
--- server.js
+++ server.js
@@ -7,6 +7,25 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function (req, res) {
+    console.log("GET req received")
+
+    person1 = {
+        name: 'Mayank',
+        email: 'mayank@gmail.com',
+        number: '123-456-789'
+    };
+
+    person2 = {
+        name: 'Joe',
+        email: 'Joe@gmail.com',
+        number: '789-456-123'
+    };
+
+    var contactlist = [person1, person2];
+    res.json(contactlist);
+});
+
 
 
 app.listen(3000);
