67de7dbcdf10d265e3b701bcc6044f727bed6e63
Mayank Gangrade <gangrademayank91@gmail.com>
2018-11-12 17:45:38 -0500

    getting data from db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 // app.get('/', function(req, res){
 //     res.send("Hello world")
@@ -10,20 +12,28 @@ app.use(express.static(__dirname + "/public"));
 app.get('/contactlist', function (req, res) {
     console.log("GET req received")
 
-    person1 = {
-        name: 'Mayank',
-        email: 'mayank@gmail.com',
-        number: '123-456-789'
-    };
+    db.contactlist.find(function(err, docs){
+        console.log(docs);
+        res.json(docs);
+    });
 
-    person2 = {
-        name: 'Joe',
-        email: 'Joe@gmail.com',
-        number: '789-456-123'
-    };
 
-    var contactlist = [person1, person2];
-    res.json(contactlist);
+
+
+    // person1 = {
+    //     name: 'Mayank',
+    //     email: 'mayank@gmail.com',
+    //     number: '123-456-789'
+    // };
+
+    // person2 = {
+    //     name: 'Joe',
+    //     email: 'Joe@gmail.com',
+    //     number: '789-456-123'
+    // };
+
+    // var contactlist = [person1, person2];
+    // res.json(contactlist);
 });
 
 
