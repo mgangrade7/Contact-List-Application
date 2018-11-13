839f58e0a146b2f83128e6c4352ebb55d71d0bf3
Mayank Gangrade <gangrademayank91@gmail.com>
2018-11-12 19:00:26 -0500

    refresh page added

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,6 +3,7 @@ myApp.controller('AppCtrl', ['$scope', '$http',
     function ($scope, $http) {
         console.log("hello world from controller");
 
+    var refresh = function(){
         $http.get('/contactlist').
             then(function (response) {
                 //success code
@@ -10,12 +11,16 @@ myApp.controller('AppCtrl', ['$scope', '$http',
                 console.log(response);
                 $scope.contactlist = response.data;
             });
+        };
+    
+        refresh();
 
         $scope.addContact = function () {
             console.log($scope.contact);
             $http.post('/contactlist', $scope.contact).
             then(function (response){
                 console.log(response.data);
+                refresh();
             });
-        }
+        };
     }]);
